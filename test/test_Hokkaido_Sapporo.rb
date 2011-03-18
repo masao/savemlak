f6e8599f3cf8be5237dae596e2a31a6a5e45226b
Masao Takaku <tmasao@acm.org>
2011-03-18 11:24:29 +0900

    rm debug message

--- test/test_Hokkaido_Sapporo.rb
+++ test/test_Hokkaido_Sapporo.rb
@@ -5,14 +5,14 @@ require "pp"
 
 require "load_atwiki.rb"
 
-class TestAtwiki < Test::Unit::TestCase
+class TestAtwiki_Hokkaido < Test::Unit::TestCase
    def test_load_atwiki_Hokaido_Sapporo
       data = load_atwiki_data( [ 
          { :name => "Hokkaido", :pref => "北海道", :wikiname => "30.html" },
       ] )
       assert( data[ "Hokkaido" ] )
       assert( data[ "Hokkaido" ].size > 0 )
-      pp data[ "Hokkaido" ]
+      #pp data[ "Hokkaido" ]
       library = data[ "Hokkaido" ].find{|e| e[:title] === "札幌市中央図書館" }
       assert( library, "There is no Sapporo Library. (It may be changed in its name.)" )
       assert( library[ :calil ], "There is no Calil info for Sapporo  Library." )
