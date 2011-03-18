4c068ff54b809f75b179e72193bb7363231de04a
Masao Takaku <tmasao@acm.org>
2011-03-18 11:10:03 +0900

    changed to use "setup" def.

--- test/test_Tochigi.rb
+++ test/test_Tochigi.rb
@@ -5,15 +5,18 @@ require "pp"
 
 require "load_atwiki.rb"
 
-class TestAtwiki < Test::Unit::TestCase
-   def test_load_atwiki_Tochigi_Univ_Sakushin
+class TestAtwiki_Tochigi < Test::Unit::TestCase
+   def setup
       data = load_atwiki_data( [ 
          { :name => "Tochigi", :pref => "栃木県", :wikiname => "26.html" },
       ] )
-      assert( data[ "Tochigi" ] )
-      assert( data[ "Tochigi" ].size > 0 )
-      #pp data[ "Tochigi" ]
-      library = data[ "Tochigi" ].find{|e| e[:title] =~ /作新学院大学/ }
+      @data = data[ "Tochigi" ]
+   end
+   def test_load_atwiki_Tochigi_Univ_Sakushin
+      assert( @data )
+      assert( @data.size > 0 )
+      #pp @data
+      library = @data.find{|e| e[:title] =~ /作新学院大学/ }
       assert( library, "There is no Sakushin Univ Library. (It may be changed in its name.)" )
       assert( library[ :calil ], "There is no Calil info for Sakushin Univ Library." )
    end
