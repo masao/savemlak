e8337fd1e6eac250f92b000e64afe32467fedf5a
Masao Takaku <tmasao@acm.org>
2011-04-03 15:27:26 +0900

    remove category yomi if it exists.

--- check_yomi.py
+++ check_yomi.py
@@ -26,15 +26,15 @@ docuReplacements = {
     '&params;': pagegenerators.parameterHelp
 }
 
-class BasicBot:
+class CheckYomiBot:
     # Edit summary message that should be used.
     # NOTE: Put a good description here, and add translations, if possible!
     msg = {
-        'en': u'Robot: Changing ...',
-        'ja':u'ロボットによる：編集',
+        'en': u'Robot: Checking Yomi info',
+        'ja':u'ロボットによる編集: check Yomi field',
     }
 
-    def __init__(self, generator, dry):
+    def __init__(self, generator, dry, always):
         """
         Constructor. Parameters:
             @param generator: The page generator that determines on which pages
@@ -46,6 +46,7 @@ class BasicBot:
         """
         self.generator = generator
         self.dry = dry
+        self.always = always
         # Set the edit summary message
         self.summary = pywikibot.translate(pywikibot.getSite(), self.msg)
 
@@ -53,10 +54,10 @@ class BasicBot:
     	self.count = { "target" : 0, "done" : 0 }
         for page in self.generator:
             self.treat(page)
-	print "Done: %.01f%% (%d/%d)" % \
-		( 100*self.count["done"] / float(self.count["target"]),
-		  self.count["done"], self.count["target"],
-		)
+        print "Done: %.01f%% (%d/%d)" % \
+              ( 100*self.count["done"] / float(self.count["target"]),
+                self.count["done"], self.count["target"],
+              )
 
     def treat(self, page):
         """
@@ -68,11 +69,47 @@ class BasicBot:
 
 	self.count[ "target" ] += 1
 	pattern = re.compile( u"\\|\\s*よみ\\s*=" )
-	if pattern.search( text ):
-	    print "%s: done." % page.title(asLink=True)
-	    self.count[ "done" ] += 1
-	else:
+	if not pattern.search( text ):
             print "%s" % page.title(asLink=True)
+            return
+	else:
+	    #print "%s done." % page.title(asLink=True)
+	    self.count[ "done" ] += 1
+
+        # Munge!
+        text = re.sub( r'\[\[Category:(.+?)\|.*?\]\]',
+                       r'[[Category:\1]]',
+                       text )
+
+        # only save if something was changed
+        if text != page.get():
+            # Show the title of the page we're working on.
+            # Highlight the title in purple.
+            pywikibot.output(u"\n\n>>> %s <<<" % page.title())
+            # show what was changed
+            pywikibot.showDiff(page.get(), text)
+            if not self.dry:
+                if not self.always:
+                    choice = pywikibot.inputChoice(
+                        u'Do you want to accept these changes?',
+                        ['Yes', 'No'], ['y', 'N'], 'N')
+                else:
+                    choice = 'y'
+                if choice == 'y':
+                    try:
+                        # Save the page
+                        page.put(text)
+                    except pywikibot.LockedPage:
+                        pywikibot.output(u"Page %s is locked; skipping."
+                                         % page.title(asLink=True))
+                    except pywikibot.EditConflict:
+                        pywikibot.output(
+                            u'Skipping %s because of edit conflict'
+                            % (page.title()))
+                    except pywikibot.SpamfilterError, error:
+                        pywikibot.output(
+                            u'Cannot change %s because of spam blacklist entry %s'
+                            % (page.title(), error.url))
 
     def load(self, page):
         """
@@ -104,11 +141,15 @@ def main():
     # If dry is True, doesn't do any real changes, but only show
     # what would have been changed.
     dry = False
+    # will become True when the user uses the -always flag.
+    always = False
 
     # Parse command line arguments
     for arg in pywikibot.handleArgs():
         if arg.startswith("-dry"):
             dry = True
+        if arg.startswith("-always"):
+            always = True
         else:
             # check if a standard argument like
             # -start:XYZ or -ref:Asdf was given.
@@ -127,7 +168,7 @@ def main():
         # The preloading generator is responsible for downloading multiple
         # pages from the wiki simultaneously.
         gen = pagegenerators.PreloadingGenerator(gen)
-        bot = BasicBot(gen, dry)
+        bot = CheckYomiBot(gen, dry, always)
         bot.run()
     else:
         pywikibot.showHelp()
