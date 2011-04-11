3a80d0a4ab2bda08342dfa57c7e7651b40af2bf0
Masao Takaku <tmasao@acm.org>
2011-04-12 01:42:48 +0900

    Geocoding with GoogleMaps.

---
+++ geocode.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+"""
+
+The following parameters are supported:
+
+&params;
+
+-dry              If given, doesn't do any real changes, but only shows
+                  what would have been changed.
+
+All other parameters will be regarded as part of the title of a single page,
+and the bot will only work on that single page.
+"""
+
+import sys
+import re
+import urllib
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+sys.path.append( "../pywikipedia/" )
+import wikipedia as pywikibot
+import pagegenerators
+
+# This is required for the text that is shown when you run this script
+# with the parameter -help.
+docuReplacements = {
+    '&params;': pagegenerators.parameterHelp
+}
+
+class GeocodeBot:
+    # Edit summary message that should be used.
+    # NOTE: Put a good description here, and add translations, if possible!
+    msg = {
+        'en': u'Robot: Geocoding',
+        'ja':u'ロボットによる編集: 緯度経度の自動取得',
+    }
+
+    def __init__(self, generator, dry, always):
+        """
+        Constructor. Parameters:
+            @param generator: The page generator that determines on which pages
+                              to work.
+            @type generator: generator.
+            @param dry: If True, doesn't do any real changes, but only shows
+                        what would have been changed.
+            @type dry: boolean.
+        """
+        self.generator = generator
+        self.dry = dry
+        self.always = always
+        # Set the edit summary message
+        self.summary = pywikibot.translate(pywikibot.getSite(), self.msg)
+
+    def run(self):
+        pywikibot.setAction( self.summary )
+        for page in self.generator:
+            self.treat(page)
+
+    def treat(self, page):
+        """
+        Loads the given page, does some changes, and saves it.
+        """
+        text = self.load(page)
+        if not text:
+            return
+
+	pattern1 = re.compile( ur'\|\s*所在地\s*=\s*(\S.*)\n' )
+        m = pattern1.search( text )
+        if not m:
+            print "Address not found: %s" % page.title(asLink=True)
+            return
+        address = m.group( 1 )
+        #print address
+
+	pattern2 = re.compile( ur'\s*\|\s*緯度経度\s*=([^\n]*)\n' )
+        m = pattern2.search( text )
+        if m:
+            val = m.group( 1 ).strip()
+            #print len(val)
+            if len(val) == 0 or re.match( ur'^0\s*,\s*0$', val ):
+                text = re.sub( pattern2, r'\n', text )
+            else:
+                return
+
+        latlon = self.geocoding( address )
+
+        text = re.sub( pattern1,
+                       ur'\g<0>|緯度経度=%s,%s\n' % ( latlon["lat"], latlon["lon"] ),
+                       text )
+
+        # only save if something was changed
+        if text != page.get():
+            # Show the title of the page we're working on.
+            # Highlight the title in purple.
+            pywikibot.output(u"\n\n>>> %s <<<" % page.title())
+            # show what was changed
+            pywikibot.showDiff(page.get(), text)
+            if not self.dry:
+                if not self.always:
+                    choice = pywikibot.inputChoice(
+                        u'Do you want to accept these changes?',
+                        ['Yes', 'No'], ['y', 'N'], 'N')
+                else:
+                    choice = 'y'
+                if choice == 'y':
+                    try:
+                        # Save the page
+                        page.put(text)
+                    except pywikibot.LockedPage:
+                        pywikibot.output(u"Page %s is locked; skipping."
+                                         % page.title(asLink=True))
+                    except pywikibot.EditConflict:
+                        pywikibot.output(
+                            u'Skipping %s because of edit conflict'
+                            % (page.title()))
+                    except pywikibot.SpamfilterError, error:
+                        pywikibot.output(
+                            u'Cannot change %s because of spam blacklist entry %s'
+                            % (page.title(), error.url))
+
+    def load(self, page):
+        """
+        Loads the given page, does some changes, and saves it.
+        """
+        try:
+            # Load the page
+            text = page.get()
+        except pywikibot.NoPage:
+            pywikibot.output(u"Page %s does not exist; skipping."
+                             % page.title(asLink=True))
+        except pywikibot.IsRedirectPage:
+            pywikibot.output(u"Page %s is a redirect; skipping."
+                             % page.title(asLink=True))
+        else:
+            return text
+        return None
+
+    def geocoding(self, address):
+        url = 'http://maps.google.com/maps/api/geocode/json?'
+        url = url + '&language=ja&sensor=false&region=ja'
+        url = url + '&address=' + urllib.quote(address.encode('utf-8'))
+        io = urllib.urlopen( url )
+        content = io.read()
+        #print "%s" % content
+        obj = json.loads(content)
+        if obj["status"] != "OK":
+            return None
+        result = {}
+        result['lon'] = str(obj["results"][0]["geometry"]["location"]["lng"])
+        result['lat'] = str(obj["results"][0]["geometry"]["location"]["lat"])
+        return result
+
+def main():
+    # This factory is responsible for processing command line arguments
+    # that are also used by other scripts and that determine on which pages
+    # to work on.
+    genFactory = pagegenerators.GeneratorFactory()
+    # The generator gives the pages that should be worked upon.
+    gen = None
+    # This temporary array is used to read the page title if one single
+    # page to work on is specified by the arguments.
+    pageTitleParts = []
+    # If dry is True, doesn't do any real changes, but only show
+    # what would have been changed.
+    dry = False
+    # will become True when the user uses the -always flag.
+    always = False
+    # will input Yomi data
+    input = False
+    # will input Yomi data
+    outputwiki = False
+
+    # Parse command line arguments
+    for arg in pywikibot.handleArgs():
+        if arg.startswith("-dry"):
+            dry = True
+        elif arg.startswith("-always"):
+            always = True
+        else:
+            # check if a standard argument like
+            # -start:XYZ or -ref:Asdf was given.
+            if not genFactory.handleArg(arg):
+                pageTitleParts.append(arg)
+
+    if pageTitleParts != []:
+        # We will only work on a single page.
+        pageTitle = ' '.join(pageTitleParts)
+        page = pywikibot.Page(pywikibot.getSite(), pageTitle)
+        gen = iter([page])
+
+    if not gen:
+        gen = genFactory.getCombinedGenerator()
+    if gen:
+        # The preloading generator is responsible for downloading multiple
+        # pages from the wiki simultaneously.
+        gen = pagegenerators.PreloadingGenerator(gen)
+        bot = GeocodeBot(gen, dry, always)
+        bot.run()
+    else:
+        pywikibot.showHelp()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
