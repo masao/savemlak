891abcf5a9427bc9430ef2d77ad22fc6cf1d7058
Masao Takaku <tmasao@acm.org>
2011-04-30 13:12:21 +0900

    extract Pref from address.

--- alist2mediawiki.rb
+++ alist2mediawiki.rb
@@ -6,6 +6,8 @@ require "kconv"
 require "rubygems"
 require "fastercsv"
 
+$KCODE = "u"
+
 if $0 == __FILE__
    cont = ARGF.read
    csv = FasterCSV.parse( cont.toutf8, { :headers => true } )
@@ -16,13 +18,13 @@ if $0 == __FILE__
       pagename = pagename.sub( /\A\((株|財)\)/, "" )
       pref = c['| 都道府県=']
       if not pref
-         if c['| 所在地='] =~ /\A(.+?[都道府県])/
+         if c['| 所在地='] =~ /\A\s*(?:〒?\d\d\d-?(?:\d\d\d\d)?)?\s*(.+?[都道府県])/
             pref = $1
          end
       end
       notes = [ c['|備考='] ]
       notes << "英名称: c['英文タイトル']}" if c['英文タイトル']
-      notes << "典拠: #{ c['典拠'] }" if c['典拠']
+      notes << "(典拠: #{ c['典拠'] })" if c['典拠']
       input_exist = [
          c['| 被害状況='],
          c['| 職員・利用者の被害='],
