e1cd21e3cb9d7b183031bd8737878ed74ae93c1e
Masao Takaku <tmasao@acm.org>
2011-05-12 09:28:13 +0900

    nocapitalize for saveMLAK (avoid WARNING)

--- savemlak_family.py
+++ savemlak_family.py
@@ -14,6 +14,7 @@ class Family(family.Family):
                 'ja': 'savemlak.jp',
                 'en': 'savemlak.jp',
         }
+	self.nocapitalize = [ 'ja', 'en' ]
         self.namespaces[1] = {
             'ja': u'トーク',
         }
