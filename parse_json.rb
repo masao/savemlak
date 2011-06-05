11afcf4f31ee798c307351ba3de50d854c3d2dac
Masao Takaku <tmasao@acm.org>
2011-06-05 09:59:41 +0900

    add a script for outcome table.

---
+++ parse_json.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# $Id$
+
+require "rubygems"
+require "json"
+
+# get_json.rb で取得した JSON ファイルを解析して統計表形式に変換する
+# 引数として、json ファイル名を指定すること
+#
+# USAGE: ./parse_json.rb *.json
+
+puts <<EOF
+{| class="wikitable sortable"
+! nowrap | 時間帯
+!編集回数
+!編集ユーザ数
+!編集ユーザ
+EOF
+ARGV.each do |f|
+   changes = JSON.load( open(f) )["query"]["recentchanges"]
+   users = Hash.new(0)
+   changes.each do |e|
+      users["[[特別:投稿記録/#{ e["user"] }|#{ e["user"] }]]"] += 1
+   end
+   puts <<EOF
+|-
+| #{ File.basename( f ).gsub(/\.json$/,"") }時台
+| #{ changes.size }
+| #{ users.size }
+| #{ users.keys.sort_by{|e| users[e] }.reverse.join(", ") }
+EOF
+end
+
+puts "|}"
