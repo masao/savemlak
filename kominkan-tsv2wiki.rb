d355b77d58274de42097dffcd74e2ff2a3865847
Masao Takaku <tmasao@acm.org>
2011-06-07 01:08:01 +0900

    Changed wrong codes and names.

--- kominkan-tsv2wiki.rb
+++ kominkan-tsv2wiki.rb
@@ -24,11 +24,27 @@ ARGF.each do |line|
    if fax1 and fax2 and fax3
       fax = [ fax1, fax2, fax3 ].join( "-" )
    end
-   puts [ identifier[0..4], LOCAL[ identifier[ 0..4 ] ], name ]
-#    open( name + ".txt", "w" ) do |io|
+   pagename = name.sub( /\A#{ PREF[ pref_code ] }(.+郡)?/, "" )
+   local_gov = LOCAL[ identifier[0..4] ]
+   local_gov2 = local_gov.gsub( /ケ/, "ヶ" )
+   #puts [local_gov, local_gov2]
+   if local_gov == local_gov2
+      local_gov2 = local_gov.gsub( /ヶ/, "ケ" )
+   end
+   if local_gov =~ /\A(.+市)(.+区)\Z/
+      local_gov2 = $1.dup
+   end
+   if pagename[ 0...local_gov.size ] != local_gov and pagename[ 0...local_gov2.size ] != local_gov2
+      #puts name[ 0...local_gov2.size ]
+      pagename = local_gov + pagename
+      #puts [ name, pagename ] if pagename[0..3] == name[0..3]
+   end
+   #puts [ identifier[0..4], LOCAL[ identifier[ 0..4 ] ], name, pagename ]
+   puts pagename
+   #open( name + ".txt", "w" ) do |io|
 #       print <<EOF
 # {{subst:新規施設
-# | 名称=#{ name }
+# | 名称=#{ pagename }
 # | 都道府県=#{ PREF[ pref_code ] }
 # | 施設種別=公民館
 # | 所在地=#{ "〒" if zipcode  }#{ zipcode } #{ address }
@@ -37,5 +53,5 @@ ARGF.each do |line|
 # | 備考=（典拠：全国公民館連合会調査・平成20年7月）
 # }}
 # EOF
-#    end
+   #end
 end
