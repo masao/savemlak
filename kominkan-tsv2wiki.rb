b3783275550c6b81e3ae3d9879bb26fa7795d22e
Masao Takaku <tmasao@acm.org>
2011-06-10 18:33:12 +0900

    output to files.

--- kominkan-tsv2wiki.rb
+++ kominkan-tsv2wiki.rb
@@ -41,18 +41,18 @@ ARGF.each do |line|
       #puts [ name, pagename ] if pagename[0..3] == name[0..3]
    end
    #puts [ identifier[0..4], LOCAL[ identifier[ 0..4 ] ], name, pagename ]
-   puts pagename
-   #open( name + ".txt", "w" ) do |io|
-#       print <<EOF
-# {{subst:新規施設
-# | 名称=#{ pagename }
-# | 都道府県=#{ PREF[ pref_code ] }
-# | 施設種別=公民館
-# | 所在地=#{ "〒" if zipcode  }#{ zipcode } #{ address }
-# | 電話番号=#{ tel }
-# | FAX=#{ fax }
-# | 備考=（典拠：全国公民館連合会調査・平成20年7月）
-# }}
-# EOF
-   #end
+   #puts pagename
+   open( name + ".txt", "w" ) do |io|
+      io.print <<EOF
+{{subst:新規施設
+| 名称=#{ pagename }
+| 都道府県=#{ PREF[ pref_code ] }
+ | 施設種別=公民館
+| 所在地=#{ "〒" if zipcode  }#{ zipcode } #{ address }
+| 電話番号=#{ tel }
+| FAX=#{ fax }
+| 備考=（典拠：全国公民館連合会調査・平成20年7月）
+}}
+EOF
+   end
 end
