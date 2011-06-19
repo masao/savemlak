401fb2af28e79e422cd0ba2c94ad95bea8486439
Masao Takaku <tmasao@acm.org>
2011-06-20 01:01:21 +0900

    get query-continue data.

--- get_json.rb
+++ get_json.rb
@@ -12,24 +12,37 @@ require "date"
 require "time"
 require "open-uri"
 
+require "rubygems"
+require "json"
+
 BASEURL = "http://savemlak.jp/savemlak/api.php?action=query&list=recentchanges&rcdir=newer&rclimit=500&rcprop=user%7Ctitle%7Ctimestamp&format=json"
 
-date = ARGV[0] ? Date.parse( ARGV[0] ) : Date.today
-puts "getting data of #{ date.to_s }..."
-time = Time.local( date.year, date.month, date.day ).gmtime
-
-#p date.to_s
-#p time
-
-24.times do |i|
-   start_t = ( time + ( i * 60 * 60 ) ).strftime( "%Y%m%d%H%M%S" )
-   end_t   = ( time + ( (i+1) * 60 * 60 ) ).strftime( "%Y%m%d%H%M%S" )
-   p [ i, start_t, end_t ]
-   json = open( BASEURL + "&rcstart=#{ start_t }&rcend=#{ end_t }
-" ) do |http|
+def get_json( start_t, end_t )
+   json = open( BASEURL + "&rcstart=#{ start_t }&rcend=#{ end_t }" ) do |http|
       http.read
    end
-   open( "#{ "%02d" % i }.json", "w" ) do |io|
-      io.print json
+end
+
+if $0 == __FILE__
+   date = ARGV[0] ? Date.parse( ARGV[0] ) : Date.today
+   puts "getting data of #{ date.to_s }..."
+   time = Time.local( date.year, date.month, date.day ).gmtime
+   #p date.to_s
+   #p time
+   24.times do |i|
+      start_t = ( time + ( i * 60 * 60 ) ).strftime( "%Y%m%d%H%M%S" )
+      end_t   = ( time + ( (i+1) * 60 * 60 ) ).strftime( "%Y%m%d%H%M%S" )
+      p [ i, start_t, end_t ]
+      json = get_json( start_t, end_t )
+      json_obj = JSON.parse( json )
+      if json_obj["query-continue"]
+         start_t2 = Time.parse( json_obj["query-continue"]["recentchanges"]["rcstart"] ).strftime( "%Y%m%d%H%M%S" )
+         json2 = get_json( start_t2, end_t )
+         json_obj["query"]["recentchanges"] += JSON.parse( json2 )["query"]["recentchanges"]
+         json = JSON.dump( json_obj )
+      end
+      open( "#{ "%02d" % i }.json", "w" ) do |io|
+         io.print json
+      end
    end
 end
