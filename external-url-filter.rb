cad17ebfe6b939418c2befe6aa0f54b2697e2721
Masao Takaku <tmasao@acm.org>
2011-08-30 17:40:04 +0900

    changed to output page_title and page_namespace.

--- external-url-filter.rb
+++ external-url-filter.rb
@@ -20,10 +20,44 @@ def parse_url( url )
    uri
 end
 
+def fullpagename( title, ns = 0 )
+   ns = ns.to_i
+   case ns
+   when 0
+      title
+   when 1
+      "Talk:" << title
+   when 2
+      "User:" << title
+   when 3
+      "User talk:" << title
+   when 4
+      "saveMLAK:" << title
+   when 5
+      "saveMLAK talk:" << title
+   when 6
+      "File:" << title
+   when 8
+      "MediaWiki:" << title
+   when 10
+      "Template:" << title
+   when 12
+      "Help:" << title
+   when 14
+      "Category:" << title
+   when 274
+      "Widget:" << title
+   when 108
+      "Concept:" << title
+   else
+      raise "Unknown namespace: #{ ns }"
+   end
+end
+
 if $0 == __FILE__
    count = {}
    ARGF.each do |line|
-      page_id, url, = line.chomp.split( /\t/ )
+      page_id, url, title, ns, = line.chomp.split( /\t/ )
       uri = nil
       begin
          uri = parse_url( url )
@@ -38,7 +72,7 @@ if $0 == __FILE__
          next
       end
       count[ url ] ||= []
-      count[ url ] << page_id
+      count[ url ] << fullpagename( title, ns )
    end
    count.keys.sort_by{|e| -1 * count[e].size }.each do |url|
       puts [ url, count[url] ].join("\t")
