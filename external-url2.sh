8d99b67576a66b108f4061a1787852e13cf048fd
Masao Takaku <tmasao@acm.org>
2011-08-30 17:40:04 +0900

    changed to output page_title and page_namespace.

--- external-url2.sh
+++ external-url2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mysql -u root -B -D savemlak -e 'select el_from, el_to from externallinks' --skip-column-names
+mysql --default-character-set=utf8 -u root -B -D savemlak -e 'select externallinks.el_from, externallinks.el_to, page.page_title, page.page_namespace from externallinks, page where el_from = page.page_id' --skip-column-names
