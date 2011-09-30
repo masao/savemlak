6177f43b2af4d4ede2dd7c563c041f1840acca5d
Masao Takaku <tmasao@acm.org>
2011-09-30 16:05:07 +0900

    add

---
+++ wikirecovery-20110930/ext-cache.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# $Id$
+
+require "uri"
+
+ARGV.each do |f|
+   html = open( f ){|io| io.read }
+   html_results = html.split( /<h3/ )
+   html_results.shift
+   html_results.each_with_index do |s,i|
+      title = nil
+      if s =~ /\A.*?<a href="([^"]*)"/
+         url = $1
+         uri = URI.parse( url )
+         if uri.respond_to?( :request_uri )
+            title = uri.request_uri.gsub( /\A\/wiki\//, "" )
+            title = URI.unescape( title )
+            cache_url = $1 if s =~ /\A.*?<a href="([^"]*)"[^>]*>キャッシュ/
+            puts [ title, url, cache_url ].join("\t")
+         else
+            puts "WARN: url is not for 'http://' (#{ uri })"
+         end
+      else
+         puts "WARN: html does not include anchor"
+      end
+   end
+end
