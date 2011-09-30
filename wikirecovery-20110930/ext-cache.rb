4f75f55eae63ed604195bc1cf90e4baea369b9fc
Masao Takaku <tmasao@acm.org>
2011-09-30 16:42:50 +0900

    unescapeHTML for urls

--- wikirecovery-20110930/ext-cache.rb
+++ wikirecovery-20110930/ext-cache.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # $Id$
 
+require "cgi"
 require "uri"
 
 ARGV.each do |f|
@@ -11,12 +12,12 @@ ARGV.each do |f|
    html_results.each_with_index do |s,i|
       title = nil
       if s =~ /\A.*?<a href="([^"]*)"/
-         url = $1
+         url = CGI.unescapeHTML( $1 )
          uri = URI.parse( url )
          if uri.respond_to?( :request_uri )
             title = uri.request_uri.gsub( /\A\/wiki\//, "" )
             title = URI.unescape( title )
-            cache_url = $1 if s =~ /\A.*?<a href="([^"]*)"[^>]*>キャッシュ/
+            cache_url = CGI.unescapeHTML( $1 ) if s =~ /\A.*?<a href="([^"]*)"[^>]*>キャッシュ/
             puts [ title, url, cache_url ].join("\t")
          else
             puts "WARN: url is not for 'http://' (#{ uri })"
