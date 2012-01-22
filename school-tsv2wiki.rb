95178b5a7aeda99f16b9944d5b89a81f3156c6a2
Masao Takaku <tmasao@acm.org>
2012-01-22 19:09:15 +0900

    changed to column based parsing

--- school-tsv2wiki.rb
+++ school-tsv2wiki.rb
@@ -4,26 +4,75 @@
 
 require "nkf"
 
+class String
+   def split_tabs
+      NKF.nkf( "-EwZ1", self ).split( /\t/ )
+   end
+end
+
+header = []
+cols = ARGF.gets.chomp.split_tabs
+cols.each do |col|
+   case col
+   when /\A\s*\|名称=/o
+      header << :name
+   when /\A\s*\|よみ=/o
+      header << :yomi
+   when /\A\s*\|所在地=/o
+      header << :address
+   when /\A\s*\|電話番号=/o
+      header << :tel
+   when /\A\s*\|FAX=/o
+      header << :fax
+   when /\A\s*\|メールアドレス=/o
+      header << :email
+   when /\A\s*\|URL=/o
+      header << :url
+   when /\A\s*\|備考=/o
+      header << :note
+   when /\A\s*\|== その他 ==/o
+      header << :others
+   end
+end
+p header
 ARGF.each do |line|
-   # |名称=	|よみ=	|所在地=	|電話番号=	|FAX=	|メールアドレス=	|URL=	|備考=	== その他 ==
-   name, yomi, address, tel, fax, email, url, note, others, *categories = NKF.nkf( "-EwZ1", line.chomp ).split( /\t/ )
-   others = others.gsub( /<br\s*\/>/i, "\n" ).strip
-   #puts others
-   #p categories
-   next if name =~ /^\|/o
-   open( name + ".txt", "w" ) do |io|
+   data = {}
+   array = line.chomp.split_tabs
+   array.each_with_index do |e, i|
+      #p header[i]
+      if header[ i ]
+         data[ header[i] ] = e
+      else
+         if e =~ /^\[\[Category:/i
+            data[ :categories ] ||= []
+            data[ :categories ] << e
+         else
+            raise "unknown column: #{ i }: #{ e }"
+         end
+      end
+   end
+   if data[ :others ]
+      data[ :others ].gsub( /<br\s*\/>/i, "\n" ).strip
+   end
+   #next
+   if not data[ :name ]
+      p data
+      next
+   end
+   #p data
+   open( data[ :name ] + ".txt", "w" ) do |io|
       io.print <<EOF
 {{施設
-|名称=#{ name }
-|よみ=#{ yomi }
-|所在地=#{ address }
+|名称=#{ data[ :name ] }
+|よみ=#{ data[ :yomi ] }
+|所在地=#{ data[ :address ] }
 |緯度経度=
-|電話番号=#{ tel }
-|FAX=#{ fax }
-|メールアドレス=#{ email }
-|URL=#{ url }
+|電話番号=#{ data[ :tel ] }
+|FAX=#{ data[ :fax ] }
+|メールアドレス=#{ data[ :email ] }
+|URL=#{ data[ :url ] }
 |Twitterアカウント=
-|備考=#{ note }
+|備考=#{ data[ :note ] }
 }}
 == 被害状況 ==
 
@@ -43,14 +92,14 @@ ARGF.each do |line|
 === 避難受入規模 ===
 
 == その他 ==
-#{ others }
+#{ data[ :others ] }
 
 == 情報源 ==
 === 記入者 ===
 
 === 元情報 ===
 
-#{ categories.join( "\n" ) }
+#{ data[ :categories ].join( "\n" ) }
 EOF
    end
 end
