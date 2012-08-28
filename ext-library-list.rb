969a547524a6128978a2b9ad4537e7f7d5e18e10
Masao Takaku <tmasao@acm.org>
2012-08-28 17:27:06 +0900

    extract prefecture name as category save wikitext for updating.

--- ext-library-list.rb
+++ ext-library-list.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+$:.push( File.dirname($0) )
+require "local_government.rb"
+
 require "rubygems"
 require "libxml"
 
@@ -14,6 +17,43 @@ if $0 == __FILE__
    pages.each do |page|
       title = page.find( "./mw:title" )[0].content
       text = page.find( "./mw:revision/mw:text" )[0].content
-      puts title if text == "ここに本文が入ります。"
+      pref = nil
+      if text == "ここに本文が入ります。"
+         PREF.each do |k, v|
+            if title.include? v
+               pref = v
+               break
+            end
+         end
+         if not pref
+            LOCAL.each do |k, v|
+               next if v.empty?
+               #p [ k, v ]
+               if title.start_with? v
+                  pref = PREF[ k[ 0, 2 ].to_i ]
+                  #puts [ title, v ].join( "\t" )
+                  break
+               end
+            end
+         end
+         cat = "[[Category:公共図書館]]"
+         if pref.nil?
+            pref = "○○県"
+            cat = nil
+         end
+
+         #puts [ title, pref, cat ].join( "\t" )
+         open( "#{ title }.txt", "w" ) do |io|
+            io.print <<EOF
+{{subst:新規施設
+| 名称=#{ title }
+| よみ=
+| 都道府県=#{ pref }
+| 施設種別=図書館
+}}
+#{ cat }
+EOF
+         end
+      end
    end
 end
